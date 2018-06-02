bfd97117e82665076a8ff6d27fb57be1973af5f3
Alexis Tacnet <alexistacnet@gmail.com>
2018-06-02 11:21:52 +0200

    Add login

--- back/src/web/auth/login.py
+++ back/src/web/auth/login.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_refresh_token_required
 
 from ..exceptions.users import *
-from .oauth import create_oauth
 
 
 def admin_only(func):
@@ -33,5 +32,22 @@ def create_auth(app):
         access_token = create_access_token(identity=current_user)
         return jsonify(access_token=access_token), 200
 
-    create_oauth(app, auth_bp)
+    @app.route('/login', methods=['POST'])
+    def login():
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"}), 400
+
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+        if not username:
+            return jsonify({"msg": "Missing username parameter"}), 400
+        if not password:
+            return jsonify({"msg": "Missing password parameter"}), 400
+
+        if username != 'test' or password != 'test':
+            return jsonify({"msg": "Bad username or password"}), 401
+
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+
     app.register_blueprint(auth_bp, url_prefix="/auth")
