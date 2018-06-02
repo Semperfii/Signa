1d3818270b321c5d2d487474180c9940b0c1c78e
Alexis Tacnet <alexistacnet@gmail.com>
2018-06-02 12:56:48 +0200

    Remove cli

--- back/run.py
+++ back/run.py
@@ -1,8 +1,8 @@
-from web import create_app, logger
+from .web import create_app, logger
 
 app = create_app()
 
 if __name__ == '__main__':
     logger.info('Starting signa API ...')
     app.run(host='0.0.0.0', port=5000, threaded=True)
-    logger.info('End of signa')
+    logger.info('End of BPLeague')
