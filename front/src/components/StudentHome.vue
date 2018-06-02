bd369b07eba0d59f9cbb85ae33aa91b6bc2f2bba
Unknown <tanguy.moullec@gmail.com>
2018-06-02 22:19:30 +0200

    Student home shitier

--- front/src/components/StudentHome.vue
+++ front/src/components/StudentHome.vue
@@ -1,32 +1,55 @@
 <template>
   <div class="main">
-    <div class="cardContainer">
-      <v-card class="cards">
-        <v-container
-          fluid
-          style="min-height: 0;"
-          grid-list-lg
-        >
-          <v-layout row wrap>
-            <v-flex xs12>
-              <v-btn color="secondary" class="white--text">
-                <div class="headline">Ouverture du quizz hebdomadaire:<br>*countdown*</div>
-              </v-btn>
-            </v-flex>
-            <v-flex xs12>
-              <v-btn color="secondary" class="white--text">
-                <div class="headline">Entraîne toi en solo</div>
-              </v-btn>
-            </v-flex>
-            <v-flex xs12>
-              <v-btn color="secondary" class="white--text">
-                <div class="headline">Défie tes amis</div>
-              </v-btn>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </div>
+    <v-container>
+      <v-layout column>
+        <v-flex xs12>
+          <v-card class="carte">
+            <v-card-title>
+              <div>
+                <h1>Quizz Hebdomadaire</h1>
+                <br>
+                <span>Améliore toi et répond en ligne aux questions proposées par ton professeur !</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat color="primary">Résultats</v-btn>
+              <v-btn flat color="primary">Jouer !</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card raised class="carte">
+            <v-card-title>
+              <div>
+                <h1>Jouer en solo</h1>
+                <br>
+                <span>Choisis ta matière et entraine toi sur une série de 10 questions à ton niveau !</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat color="primary">S'entrainer !</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card  class="carte">
+            <v-card-title>
+              <div>
+                <h1>Joue contre tes amis</h1>
+                <br>
+                <span>Choisis une matière et montre à tes amis que tu es meilleur qu'eux !</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat color="primary">Défier un ami !</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -41,27 +64,7 @@ export default {
 </script>
 
 <style>
-.cards {
-  margin-top: 10vh;
-}
-
-.cardContainer {
-  width: 50vw;
-  margin: auto;
-}
-
-.white--text {
-  width: 48vw;
-  height: 20vh;
-}
-
-.dialog {
-  display: flex;
-  justify-content: space-around;
-  width: 28vw;
-}
-
-.icon {
-  margin-left: 1vw;
+.carte {
+  margin-top: 20px;
 }
 </style>
