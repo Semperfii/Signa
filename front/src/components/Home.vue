a58b280a7e431b55f5cb48424af8ba331af4549a
beber92 <antoine.bertrand@student.ecp.fr>
2018-06-02 11:26:28 +0200

    begin auth page

---
+++ front/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <v-container>
+      <v-layout column>
+        <v-flex>
+          <v-text-field
+            id="username"
+            name="username"
+            label="Identifiant"
+            prepend-icon=
+            v-model="username"
+          ></v-text-field>
+          <v-text-field
+            id="password"
+            name="password"
+            label="Label Text"
+            prepend-icon=
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
